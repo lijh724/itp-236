3fd2129f051af09c48deb0f34a584e25b1adbc02
Jin Li <69916725+lijh724@users.noreply.github.com>
2024-01-30 01:31:23 -0500

    Add files via upload

---
+++ ShapesAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ShapesAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args) // main method
+        {
+            Rectangle r1 = new Rectangle(20, 25);   // creates Rectangle object r1; Rectangle(l,w)
+
+            ShowShape(r1);  // describes Rectangle r1 object
+
+
+            Square s1 = new Square(5);  // creates Square object s1; Square(l)
+
+            ShowShape(s1);  // describes Square s1 object
+
+
+            var s2 = new Square();  // creates Square object s1 using var
+
+            ShowShape(s2);  // describes Square s2 object
+
+
+            Circle c1 = new Circle(10); // creates Circle object c1
+
+            ShowShape(c1);  // describes Circle c1 object
+
+
+            ReadKey();  // press key to exit console
+        } // ends main
+
+        static void ShowShape(Shape s)  // catches a shape
+        {
+            // s.GetType().Name identifies the shape name
+            WriteLine($"{s.GetType().Name}\t\tArea = {s.Area}\t\tPerimeter = {s.Perimeter}\t\tUOM = {s.UoM}"); // \t is adding tabs to console display
+
+            WriteLine();    // empty line
+        } // ends ShowShape
+    }
+}
---
+++ ShapesAssignment/Shape.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesAssignment
+{
+    public abstract class Shape
+    {
+        // constructor
+        public Shape()
+        {
+            UoM = "inches";
+        }
+
+        public string UoM { get; set; }     // UoM = unit of measure
+
+        public abstract double Area { get; }    // override in class
+
+        public abstract double Perimeter { get; }   // override in class
+    } // ends Shape
+
+    // Rectangle object definition
+    public class Rectangle : Shape
+    {
+        // constructor
+        public Rectangle(double l, double w) : base()
+        {
+            Length = l;
+            Width = w;
+        }
+
+        // default constructor; this is passing the Rectangle(l,w)
+        public Rectangle() : this(1,1)
+        {
+            
+        }
+
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+        public override double Area { get { return Length * Width; } }  // override Area
+
+        public override double Perimeter { get { return (Length * Width) * 2; } }   // override Perimeter
+    }
+
+    // Square object definition
+    public class Square : Shape
+    {
+        // constructor
+        public Square(double l) : base()
+        {
+            Length = l;
+        }
+
+        // default constructor
+        public Square() : this(1)
+        {
+
+        }
+
+        public override double Area { get { return Length * Length; } } // override Area
+
+        public override double Perimeter { get { return Length * 4; } } // override Perimeter
+
+        public double Length { get; set; }
+    }
+
+    // Circle object definition
+    public class Circle : Shape
+    {
+        // constructor
+        public Circle(double r) : base()
+        {
+            Radius = r;
+        }
+
+        // default constructor
+        public Circle() : this(1)
+        {
+
+        }
+
+        public double Radius { get; set; }
+
+        public override double Area { get { return Math.PI * Radius * Radius; } }   // override Area
+
+        public override double Perimeter { get { return 2 *  Math.PI * Radius; } }  // override Perimeter
+    }
+}
