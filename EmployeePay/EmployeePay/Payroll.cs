a9bc0ae7c7064a3d4192825ab3c1d0502cdf0964
Jin Li <69916725+lijh724@users.noreply.github.com>
2024-02-06 05:53:24 -0500

    Add files via upload

---
+++ EmployeePay/EmployeePay/Payroll.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace EmployeePay
+{
+    public abstract class Employee
+    {
+        public Employee() { } // empty constructor
+
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HireDate { get; set; }
+        public abstract double Pay { get; } // override in class
+    }
+
+    public class Salary : Employee
+    {
+        public Salary() { } // empty constructor
+
+        public double MonthlySalaryAmount { get; set; }
+        public double Bonus { get; set; }
+        public override double Pay // salaried employees paid twice a month
+        {
+            get
+            {
+                return (MonthlySalaryAmount / 2) + Bonus;
+            }
+        }
+    }
+
+    public class Sales : Employee
+    {
+        public Sales() { } // empty constructor
+
+        public double Draw { get; set; }
+        public double CommissionRate { get; set; }
+        public double GrossSales { get; set; }
+        public override double Pay
+        {
+            get
+            {
+                if (CommissionRate * GrossSales >= Draw)
+                {
+                    return CommissionRate * GrossSales;
+                }
+                else
+                {
+                    return Draw;
+                }
+            }
+        }
+    }
+
+    public class Hourly : Employee
+    {
+        public Hourly() { } // empty constructor
+
+        public double Hours { get; set; }
+        public double HourlyRate { get; set; }
+        public override double Pay
+        {
+            get
+            {
+                if (Hours <= 40)
+                {
+                    return HourlyRate * Hours;
+                }
+                else
+                {
+                    return (HourlyRate * Hours) + (1.5 * HourlyRate * Hours); // overtime pay
+                }
+            }
+        }
+    }
+}
---
+++ EmployeePay/EmployeePay/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace EmployeePay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new List<Employee>();
+            employees.AddRange(LoadHourly());
+            employees.AddRange(LoadSales());
+            employees.AddRange(LoadSalary());
+            WriteLine($"Type\tEmployee\tHire Date\tPay");
+            WriteLine($"------\t----------\t-----------\t--------");
+            foreach (var employee in employees)
+                DisplayEmployee(employee);
+
+            ReadKey();
+        }
+
+        static void DisplayEmployee(Employee emp)
+        {
+            var type = emp.GetType();
+
+            WriteLine($"{type.Name}\t{emp.FirstName} {emp.LastName}\t{emp.HireDate:d}\t{emp.Pay:c}");
+        }
+        static List<Hourly> LoadHourly()
+        {
+            var hourlies = new List<Hourly>();
+
+            hourlies.Add(new Hourly()
+            {
+                EmployeeId = 101,
+                FirstName = "James",
+                LastName = "River",
+                HireDate = new DateTime(1988, 06, 07),
+                Hours = 39.5,
+                HourlyRate = 10
+            });
+            hourlies.Add(new Hourly()
+            {
+                EmployeeId = 102,
+                FirstName = "Hugh",
+                LastName = "Gaknot",
+                HireDate = new DateTime(1980, 04, 17),
+                Hours = 40,
+                HourlyRate = 11
+            });
+            hourlies.Add(new Hourly()
+            {
+                EmployeeId = 103,
+                FirstName = "Maggie",
+                LastName = "Walker",
+                HireDate = new DateTime(1977, 07, 27),
+                Hours = 41,
+                HourlyRate = 12
+            });
+
+            return hourlies;
+        }
+        static List<Sales> LoadSales()
+        {
+            var sales = new List<Sales>();
+
+            sales.Add(new Sales()
+            {
+                EmployeeId = 201,
+                FirstName = "Skip",
+                LastName = "Wythe",
+                HireDate = new DateTime(1988, 06, 07),
+                Draw = 1200,
+                CommissionRate = 8.5,
+                GrossSales = 12000
+            });
+            sales.Add(new Sales()
+            {
+                EmployeeId = 202,
+                FirstName = "Pat",
+                LastName = "Terson",
+                HireDate = new DateTime(1980, 04, 17),
+                Draw = 1500,
+                CommissionRate = 7.5,
+                GrossSales = 15000
+            });
+            sales.Add(new Sales()
+            {
+                EmployeeId = 203,
+                FirstName = "Chip",
+                LastName = "Enham",
+                HireDate = new DateTime(1977, 07, 27),
+                Draw = 0,
+                CommissionRate = 12.5,
+                GrossSales = 20000
+            });
+
+            return sales;
+        }
+        static List<Salary> LoadSalary()
+        {
+            var salaries = new List<Salary>();
+
+            salaries.Add(new Salary()
+            {
+                EmployeeId = 301,
+                FirstName = "Glen",
+                LastName = "Syde",
+                HireDate = new DateTime(1988, 06, 07),
+                MonthlySalaryAmount = 2200,
+                Bonus = 505.05
+            });
+            salaries.Add(new Salary()
+            {
+                EmployeeId = 302,
+                FirstName = "Henri",
+                LastName = "Koh",
+                HireDate = new DateTime(1980, 04, 17),
+                MonthlySalaryAmount = 3250,
+                Bonus = 750.75
+            });
+            salaries.Add(new Salary()
+            {
+                EmployeeId = 303,
+                FirstName = "Jay",
+                LastName = "Sarge",
+                HireDate = new DateTime(1977, 07, 27),
+                MonthlySalaryAmount = 5000,
+                Bonus = 0
+            });
+
+            return salaries;
+        }
+    }
+}
